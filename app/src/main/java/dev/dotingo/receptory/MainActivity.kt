b1bf403f1cc4259560638d1abae4df93fdc46a16
Dotingo <m9132824@gmail.com>
2025-01-23 14:20:58 +0300

    added splash screen

--- app/src/main/java/dev/dotingo/receptory/MainActivity.kt
+++ app/src/main/java/dev/dotingo/receptory/MainActivity.kt
@@ -4,39 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import dev.dotingo.receptory.data.Recipe
 import dev.dotingo.receptory.navigation.TopAppNavHost
-import dev.dotingo.receptory.presentation.screens.main_screen.RecipeCard
+import dev.dotingo.receptory.presentation.MainViewModel
 import dev.dotingo.receptory.ui.theme.ReceptoryTheme
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { !viewModel.isFirstLaunchLoaded.value }
         enableEdgeToEdge()
         setContent {
             ReceptoryTheme {
-                TopAppNavHost()
+                TopAppNavHost(viewModel = viewModel)
             }
+        }
+        lifecycleScope.launch {
+            viewModel.initData()
         }
     }
 }

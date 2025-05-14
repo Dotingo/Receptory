7e5db7c54396bd42a495415f0d5052c6e5195f02
Dotingo <m9132824@gmail.com>
2025-05-14 13:03:21 +0300

    bug fix

--- app/src/main/java/dev/dotingo/receptory/constants/FirebaseConstants.kt
+++ app/src/main/java/dev/dotingo/receptory/constants/FirebaseConstants.kt
@@ -3,7 +3,6 @@ package dev.dotingo.receptory.constants
 object FirebaseConstants {
     const val RECIPES = "recipes"
     const val CATEGORIES = "categories"
-    const val USERS = "users"
     const val USER_ID_FIELD = "userId"
     const val RECIPE_IMAGES = "recipeImages"
 }
